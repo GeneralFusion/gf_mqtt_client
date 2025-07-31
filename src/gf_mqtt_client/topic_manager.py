3064493d4b09e3a83a850d6ec595044d55f9460c
Matthew Davidson <122948876+generalmattza@users.noreply.github.com>
2025-07-31 12:46:06 -0700

    Bump version to 0.0.9 in pyproject.toml and update TopicManager to use MessageType for topic construction

--- src/gf_mqtt_client/topic_manager.py
+++ src/gf_mqtt_client/topic_manager.py
@@ -1,16 +1,16 @@
 import logging
 from typing import Optional
 from aiomqtt import Topic
+from .models import MessageType
 
-REQUEST_TOPIC = "request"
-RESPONSE_TOPIC = "response"
+logger = logging.getLogger(__name__)
 
 class TopicManager:
     def __init__(self, namespace: str = "gf_int_v1"):
         self.namespace = namespace
 
     def build_request_topic(self, target_device_tag: str, subsystem: str, request_id: str) -> str:
-        return f"{self.namespace}/{subsystem}/{REQUEST_TOPIC}/{target_device_tag}/{request_id}"
+        return f"{self.namespace}/{subsystem}/{MessageType.REQUEST.value}/{target_device_tag}/{request_id}"
 
     def build_response_topic(self, request_topic: str | Topic, target_device_tag: Optional[str] = None) -> str:
         if isinstance(request_topic, Topic):
@@ -19,26 +19,28 @@ class TopicManager:
         request_id = parts[-1]
         subsystem = parts[1]
         target_device_tag = target_device_tag or parts[3]
-        logging.debug(f"Building response topic for request_id: {request_id}, subsystem: {subsystem}, target_device_tag: {target_device_tag}")
-        return f"{self.namespace}/{subsystem}/{RESPONSE_TOPIC}/{target_device_tag}/{request_id}"
-    
+        logger.debug(
+            f"Building response topic for request_id: {request_id}, subsystem: {subsystem}, target_device_tag: {target_device_tag}"
+        )
+        return f"{self.namespace}/{subsystem}/{MessageType.RESPONSE.value}/{target_device_tag}/{request_id}"
+
     def get_target_device_tag_from_topic(self, request_topic: str | Topic) -> Optional[str]:
         if isinstance(request_topic, Topic):
             request_topic = request_topic.value
         parts = request_topic.split('/')
         if len(parts) < 4:
-            logging.warning(f"Invalid request topic format: {request_topic}")
+            logger.warning(f"Invalid request topic format: {request_topic}")
             return None
         target_device_tag = parts[3]
-        logging.debug(f"Extracted target device tag from request topic: {target_device_tag}")
+        logger.debug(f"Extracted target device tag from request topic: {target_device_tag}")
         return target_device_tag
-    
+
     def get_parts(self, topic: str | Topic) -> dict:
         if isinstance(topic, Topic):
             topic = topic.value
         parts = topic.split('/')
         if len(parts) < 4:
-            logging.warning(f"Invalid topic format: {topic}")
+            logger.warning(f"Invalid topic format: {topic}")
             return {}
         return {
             "namespace": parts[0],
